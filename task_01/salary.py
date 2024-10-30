f301018b38cc1a936ee2a436ca35e389dd47feb2
Oleksandr Shyian <shyian.code@gmail.com>
2024-10-30 16:34:30 +0200

    fix variables names for task_01

--- task_01/salary.py
+++ task_01/salary.py
@@ -11,13 +11,13 @@ def total_salary(path):
                     count_developers += 1
                 except ValueError:
                     print(f"Неправильний формат даних у рядку: {line.strip()}")
-            average = total / count_developers if count_developers else 0
-            return total, average
+            average = salary_total / count_developers if count_developers else 0
+            return salary_total, average
 
     except FileNotFoundError:
         print(f"Файл за шляхом '{path}' не знайдено.")
         return 0, 0
 
 # Використання функції
-total, average = total_salary("task_01/salary_file.txt")
-print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+salary_total, average = total_salary("task_01/salary_file.txt")
+print(f"Загальна сума заробітної плати: {salary_total}, Середня заробітна плата: {average}")
